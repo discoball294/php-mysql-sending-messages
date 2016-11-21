1bcb9b057a7b5695289c5637e591287e354ae5a3
bryankristian <bryan.kristian478@gmail.com>
2016-11-21 19:51:23 +0700

    Changed title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Login / Register</title>
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="css/flat-ui.min.css" rel="stylesheet" type="text/css">
     <link href="css/style-custom.css" rel="stylesheet" type="text/css">
